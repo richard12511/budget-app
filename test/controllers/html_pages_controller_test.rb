787bdf3c33134fa2f8e33a80efbf3f050b4da2ca
Richard Schmidt <richardnew12511@gmail.com>
2021-06-01 13:30:45 -0500

    add tests

--- test/controllers/html_pages_controller_test.rb
+++ test/controllers/html_pages_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class HtmlPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get home" do
+    get root_path
+    assert_response :success
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+  end
+
+  test "should get help" do
+    get help_path
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Bottom Line Budgeting | Contact"
+  end
 end
