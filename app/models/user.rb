c09cca7d0d50ac563ca6928b288bae6fea04d19c
Richard Schmidt <richardnew12511@gmail.com>
2021-06-01 15:34:24 -0500

    add uniqueness constraint

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+    before_save { self.email = email.downcase }
     validates :name, presence: true, length: { maximum: 60}
     validates :email, presence: true, length: { maximum: 255 }
     validates :email, presence: true, length: { maximum: 255 }, 
         format: { with: VALID_EMAIL_REGEX }, uniqueness: true,
-        uniqueness: { case_sensitive: false }
+        uniqueness: true
 end
