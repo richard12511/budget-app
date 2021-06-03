01cd0f065c1568cc2ed9097166736b9d1f436bcd
Richard Schmidt <richardnew12511@gmail.com>
2021-06-03 12:07:25 -0500

    fix tests

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: " ", email: "tony@example", password: "pass", password_confirmation: "pass"} }
@@ -13,6 +14,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "Ted"
