a768dc569f5d6a57de825cdfcd3a8be66c606743
Richard Schmidt <richardnew12511@gmail.com>
2021-06-03 12:17:28 -0500

    add test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,8 +11,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should redirect to edit when not logged in" do
+  test "should redirect edit when not logged in" do
     get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name, email: @user.email }}
     assert_not flash.empty?
     assert_redirected_to login_url
   end
