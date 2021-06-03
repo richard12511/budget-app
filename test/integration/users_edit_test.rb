53d422d3f5859fdeaa337cce570f1a2c0c64de9d
Richard Schmidt <richardnew12511@gmail.com>
2021-06-03 11:19:23 -0500

    add positive test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -11,4 +11,17 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     patch user_path(@user), params: { user: { name: " ", email: "tony@example", password: "pass", password_confirmation: "pass"} }
     assert_template 'users/edit'
   end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Ted"
+    email = "ted@example.com"
+    patch user_path(@user), params: { user: { name: name, email: email, password: "password123" password_confirmation: "password123"}}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
