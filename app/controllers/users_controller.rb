69bde4c7f0f077198957f90a8726c5d7ae73d0d0
Richard Schmidt <richardnew12511@gmail.com>
2021-06-03 11:41:34 -0500

    only logged in users can edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -33,6 +34,13 @@ class UsersController < ApplicationController
   end
 
   private
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
+    end
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
