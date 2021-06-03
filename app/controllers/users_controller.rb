69c53076bdaaee183e01f8234def4ab3cedb0143
Richard Schmidt <richardnew12511@gmail.com>
2021-06-03 12:13:56 -0500

    add test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  
   def new
     @user = User.new
   end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:tony)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
+  end
+
+  test "should redirect to edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 end
