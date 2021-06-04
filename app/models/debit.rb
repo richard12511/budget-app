acd844717c27f7c2a4c5fecb0d420df25685bc76
Richard Schmidt <richardnew12511@gmail.com>
2021-06-04 10:57:18 -0500

    add model tests

--- app/models/debit.rb
+++ app/models/debit.rb
@@ -1,3 +1,7 @@
 class Debit < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :amount, presence: true
+  validates :currency, presence: true
+  validates :frequency, presence: true
 end
--- test/models/debit_test.rb
+++ test/models/debit_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class DebitTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = users(:tony)
+
+    @debit = Debit.new(currency: "USD", frequency: "One Time", remarks: "test", amount: 9.99, user_id: @user.id )
+  end
+
+  test "should be valid" do
+    assert @debit.valid?
+  end
+
+  test "user id should be present" do
+    @debit.user_id = nil
+    assert_not @debit.valid?
+  end
+
+  test "amount should be present" do
+    @debit.amount = nil
+    assert_not @debit.valid?
+  end
+
+  test "currency should be present" do
+    @debit.currency = nil
+    assert_not @debit.valid?
+  end
+
+  test "frequency should be present" do
+    @debit.frequency = nil
+    assert_not @debit.valid?
+  end
 end
