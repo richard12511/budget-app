53e16711b3e86228f944f846fe257d6b582b94ce
Richard Schmidt <richardnew12511@gmail.com>
2021-06-04 11:26:38 -0500

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(name: "Test User", email: "test@testuser.com", password: "password123", password_confirmation: "password123")
+admin = User.create(name: "Admin", email: "admin@example.com", password: "password123", password_confirmation: "password123")
 
 # Bulk generate test users
 99.times do |n|
@@ -13,4 +14,15 @@ User.create!(name: "Test User", email: "test@testuser.com", password: "password1
   email = "example-#{n+1}@testuser.com"
   password = "password123"
   User.create!(name: name, email: email, password: password, password_confirmation: password)
+end
+
+users = User.all
+users.each do |user|
+  debit_count = Faker::Number.within(range: 1..10)
+
+  debit_count.times do |n|
+    amt = Faker::Number.decimal(l_digits: 2)
+    rmk = Faker::ChuckNorris.fact
+    Debit.create!(frequency: "One time", currency: "USD", remarks: rmk, amount: amt, user_id: user.id)
+  end
 end
