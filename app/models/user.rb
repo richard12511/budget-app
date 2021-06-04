2b946d4608423de613e3b98643b9ed4968d69863
Richard Schmidt <richardnew12511@gmail.com>
2021-06-04 11:44:33 -0500

    refactor seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :debits
+    
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
     before_save { self.email = email.downcase }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 User.create!(name: "Test User", email: "test@testuser.com", password: "password123", password_confirmation: "password123")
-admin = User.create(name: "Admin", email: "admin@example.com", password: "password123", password_confirmation: "password123")
+admin = User.create(name: "Admin", email: "admin@example.com", password: "password123", password_confirmation: "password123", admin: true)
 
-# Bulk generate test users
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@testuser.com"
@@ -23,6 +15,7 @@ users.each do |user|
   debit_count.times do |n|
     amt = Faker::Number.decimal(l_digits: 2)
     rmk = Faker::ChuckNorris.fact
-    Debit.create!(frequency: "One time", currency: "USD", remarks: rmk, amount: amt, user_id: user.id)
+    # Debit.create!(frequency: "One time", currency: "USD", remarks: rmk, amount: amt, user_id: user.id)
+    user.debits.create!(frequency: "One time", currency: "USD", remarks: rmk, amount: amt)
   end
 end
