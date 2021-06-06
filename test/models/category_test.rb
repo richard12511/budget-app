cf5593eae1565440dd498ab2c7a663b3b3f71f60
Richard Schmidt <richardnew12511@gmail.com>
2021-06-06 11:52:19 -0500

    add validations

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:tony)
+    @category = Category.new(category_name: "Entertainment", is_system: true, user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @category.valid?
+  end
+
+  test "user id should be present" do
+    @category.user_id = nil
+    assert_not @category.valid?
+  end
+
+  test "category name should be present" do
+    @category.category_name = "   "
+    assert_not @category.valid?
+  end
 end
